4afa3a0b25f5ed129f786270fabed9a4cd125bc6
Matt McGill <matt.mcgill@gmail.com>
2011-07-24 13:16:03 -0400

    Return chatty to its original purpose.
    The 'chatty' plugin was originally meant to
    intercept all chat messages from the server and
    dump them to the console instead. It had been
    modified to test plugin packet injection. This
    commit reverts chatty to its original purpose.

--- plugin/chatty.py
+++ plugin/chatty.py
@@ -5,18 +5,12 @@ logger = logging.getLogger('plugin.chatty')
 
 def init(sock1, sock2, args):
     """Initialize the plugin."""
-    global cli_sock, srv_sock
-    cli_sock = sock1
-    srv_sock = sock2
     logger.info("chatty plugin initialized.")
 
 def msg03(msg, dir):
     """Intercept and print chat messages."""
     if "server" == dir:
-        return True
-    msgtxt = msg['chat_msg']
-    logger.info("%s: %s", dir, msg['chat_msg'])
-    logger.debug("  raw bytes: %s", repr(msg['raw_bytes']))
-    cli_sock.inject_msg({'msgtype': 0x03, 'chat_msg': msgtxt+"!"})
-    return False
+        print msg['chat_msg']
+        return False
+    return True
 
