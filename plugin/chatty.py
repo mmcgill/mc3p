c7f565437b134e80f6366a49c72e55894822c1cb
Matt McGill <matt.mcgill@gmail.com>
2011-10-07 14:10:31 -0400

    Rewrite chatty for new Plugin API

--- plugin/chatty.py
+++ plugin/chatty.py
@@ -1,16 +1,20 @@
 
+from plugins import MC3Plugin, msghdlr
 import logging
 
 logger = logging.getLogger('plugin.chatty')
 
-def init(sock1, sock2, args):
-    """Initialize the plugin."""
-    logger.info("chatty plugin initialized.")
+class ChattyPlugin(MC3Plugin):
+    def init(self, args):
+        """Initialize the plugin."""
+        print 'chatty: %s' % args
+        logger.info("chatty plugin initialized: %s" % args)
 
-def msg03(msg, dir):
-    """Intercept and print chat messages."""
-    if "server" == dir:
-        print msg['chat_msg']
-        return False
-    return True
+    @msghdlr(0x03)
+    def handle_chat(self, msg, dir):
+        """Intercept and print chat messages."""
+        if "server" == dir:
+            print msg['chat_msg']
+            return False
+        return True
 
