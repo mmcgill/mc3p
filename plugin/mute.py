726d7d87a4f5362c63eaccebdbeb94c317d2af20
Matt McGill <matt.mcgill@gmail.com>
2011-10-17 19:44:46 -0400

    Refactor mute.py for readability

--- plugin/mute.py
+++ plugin/mute.py
@@ -16,26 +16,30 @@ class MutePlugin(MC3Plugin):
         """Send a chat message to the client."""
         self.to_client({'msgtype': 0x03, 'chat_msg': chat_msg})
 
+    def mute(self, player_name):
+        self.muted_set.add(player_name)
+        self.send_chat('Muted %s' % player_name)
+
+    def unmute(self, player_name):
+        if player_name in self.muted_set:
+            self.muted_set.remove(player_name)
+            self.send_chat('Unmuted %s' % player_name)
+        else:
+            self.send_chat('%s is not muted' % player_name)
+
+    def muted(self):
+        self.send_chat('Currently muted: %s' % ', '.join(self.muted_set))
+
     @msghdlr(0x03)
     def handle_chat(self, msg, source):
         txt = msg['chat_msg']
         if source == 'client':
             # Handle mute commands
-            if txt.startswith('/mute '):
-                player_name = txt[len('/mute '):]
-                self.muted_set.add(player_name)
-                self.send_chat('Muted %s' % player_name)
-            elif txt.startswith('/unmute '):
-                player_name = txt[len('/unmute '):]
-                if player_name in self.muted_set:
-                    self.muted_set.remove(player_name)
-                    self.send_chat('Unmuted %s' % player_name)
-                else:
-                    self.send_chat('%s is not muted' % player_name)
-            elif txt == '/muted':
-                self.send_chat('Currently muted: %s' % ', '.join(self.muted_set))
-            else:
-                return True # Forward all other chat messages.
+            if txt.startswith('/mute '):     self.mute(txt[len('/mute '):])
+            elif txt.startswith('/unmute '): self.unmute(txt[len('/unmute '):])
+            elif txt == '/muted':            self.muted()
+            else: return True # Forward all other chat messages.
+
             return False # Drop mute plugin commands.
         else:
             # Drop messages containing the string <NAME>, where NAME is a muted player name.
